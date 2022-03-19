fb240d7079aec1fd120a25bbf4e857f0fb70a941
rfdzhifa <rfdnazhifah@gmail.com>
2022-03-19 13:54:30 +0700

    google-services

---
+++ app/src/main/java/com/example/itungin_xirpl5/class/Transaction.kt
@@ -0,0 +1,7 @@
+package com.example.itungin_xirpl5.`class`
+
+class Users (var type : String, var title : String, var desc : String, var nominal : Int) {
+    constructor() : this ("", "", "", 1234) {
+
+    }
+}
