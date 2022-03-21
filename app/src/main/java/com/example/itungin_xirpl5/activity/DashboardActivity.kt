310bc0c5fe9e543bfc5334d6b1380aa7ddfce8ea
ilhamnr1610 <ilhamramadhan16104@gmail.com>
2022-03-21 08:34:37 +0700

    Ilham

--- app/src/main/java/com/example/itungin_xirpl5/activity/DashboardActivity.kt
+++ app/src/main/java/com/example/itungin_xirpl5/activity/DashboardActivity.kt
@@ -5,14 +5,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
+import android.widget.ListView
 import com.example.itungin_xirpl5.R
-import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.database.*
 
 class DashboardActivity : AppCompatActivity() {
+
+    lateinit var ref : DatabaseReference
+    lateinit var list : MutableList<Users>
+    lateinit var listView: ListView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        ref = FirebaseDatabase.getInstance().getReference("USERS")
+        list = mutableListOf()
+        listView = findViewById(R.id.listView)
+
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError) {
+            }
+
+            override fun onDataChange(p0: DataSnapshot) {
+                if (p0!!.exists()){
+
+                    for (h in p0.children){
+                        val user = h.getValue(Users::class.java)
+                        list.add(user!!)
+                    }
+                    val adapter = Adapter(applicationContext,R.layout.users,list)
+                    listView.adapter = adapter
+                }
+            }
+        })
 
         supportActionBar?.hide()
         window.setFlags(
@@ -28,4 +54,6 @@ class DashboardActivity : AppCompatActivity() {
         val intent = Intent(this, TransactionActivity::class.java)
         startActivity(intent)
     }
+
+
 }
--- app/src/main/java/com/example/itungin_xirpl5/activity/Users.kt
+++ app/src/main/java/com/example/itungin_xirpl5/activity/Users.kt
@@ -1,8 +1,8 @@
 package com.example.itungin_xirpl5.activity
 
-class Users (var title : String, var desc : String, var nominal : String) {
+class Users(var title: String, var desc: String, var nominal: Int) {
 
-    constructor() : this("", "","") {
+    constructor() : this("", "",0) {
     }
 }
 
